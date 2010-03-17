514597bba727e2eb178979ab2b23ddc58a2dcf4a
oscvam <oscvam@35220d00-f9cb-11de-91aa-85f76f42a988>
2010-03-17 17:06:23 +0000

    Добавлен новый модуль оплаты LiqPay

---
+++ trunk/bts/includes/languages/english/modules/payment/liqpay.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  $Id: webmoney_merchant.php 1778 2008-01-09 23:37:44Z hpdl $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2008 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_LIQPAY_TEXT_TITLE', 'LiqPAY');
+  define('MODULE_PAYMENT_LIQPAY_TEXT_PUBLIC_TITLE', 'LiqPAY');
+  define('MODULE_PAYMENT_LIQPAY_TEXT_DESCRIPTION', '');
+  define('MODULE_PAYMENT_LIQPAY_TEXT_ADMIN_DESCRIPTION', '');
+
+?>
