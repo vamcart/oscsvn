b09b1939c7528553d0957ec7623fc6186fb17ef8
oscvam <oscvam@35220d00-f9cb-11de-91aa-85f76f42a988>
2010-03-20 09:16:11 +0000

    Исправлена ошибка на странице обратной связи, подробнее об ошибке http://bugs.kypi.ru/view.php?id=662

--- trunk/sts/contact_us.php
+++ trunk/sts/contact_us.php
@@ -31,15 +31,15 @@ $_POST['name'] = $_POST['name'] = preg_replace( $sanita, " ", $_POST['name'] );
 	if (CONTACT_US_LIST !=''){
 		$send_to_array=explode("," ,CONTACT_US_LIST);
 
-for ( $send_to = 0; $send_to < count($send_to_array); $send_to++) {
-                
-		preg_match('/\<[^>]+\>/', $send_to_array[$send_to], $send_email_array);
+//for ( $send_to = 0; $send_to < count($send_to_array); $send_to++) {
+
+		preg_match('/\<[^>]+\>/', $send_to_array[$_POST['send_to']], $send_email_array);
 		$send_to_email= preg_replace ("/>/", "", $send_email_array[0]);
 		$send_to_email= preg_replace ("/</", "", $send_to_email);
 
-		tep_mail(preg_replace('/\<[^*]*/', '', $send_to_array[$send_to]), $send_to_email, EMAIL_SUBJECT, $enquiry, $name, $email_address);
+		tep_mail(preg_replace('/\<[^*]*/', '', $send_to_array[$_POST['send_to']]), $send_to_email, EMAIL_SUBJECT, $enquiry, $name, $email_address);
 
-}
+//}
 
 	}else{
       		tep_mail(STORE_OWNER, STORE_OWNER_EMAIL_ADDRESS, EMAIL_SUBJECT, $enquiry, $name, $email_address);
