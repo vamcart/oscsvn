b8324bdcad30f59402fbae8b5bd93761912a7dfc
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2010-05-27 15:35:44 +0000

    Обновлена страница настроек новостей и faq, сделана в виде одной формы

--- trunk/bts/admin/faqdesk_configuration.php
+++ trunk/bts/admin/faqdesk_configuration.php
@@ -1,28 +1,27 @@
 <?php
 
-require('includes/application_top.php');
+  require('includes/application_top.php');
 
-if ($_GET['action']) {
-	switch ($_GET['action']) {
-	case 'save':
-		$configuration_value = tep_db_prepare_input($_POST['configuration_value']);
-		$cID = tep_db_prepare_input($_GET['cID']);
+  if ($_GET['action']) {
+    switch ($_GET['action']) {
+      case 'save':
+$configuration_value = tep_db_prepare_input($_POST['configuration_value']);
+$cID = tep_db_prepare_input($_GET['cID']);
 
-		tep_db_query(
-		"update " . TABLE_FAQDESK_CONFIGURATION . " set configuration_value = '" . tep_db_input($configuration_value) . 
-		"', last_modified = now() where configuration_id = '" . tep_db_input($cID) . "'"
-		);
 
-		tep_redirect(tep_href_link(FILENAME_FAQDESK_CONFIGURATION, 'gID=' . $_GET['gID'] . '&cID=' . $cID));
-		break;
-	}
-}
+          $configuration_query = tep_db_query("select configuration_key,configuration_id, configuration_value, use_function,set_function from " . TABLE_FAQDESK_CONFIGURATION . " where configuration_group_id = '" . (int)$_GET['gID'] . "' order by sort_order");
 
-$cfg_group_query = tep_db_query(
-"select configuration_group_key, configuration_group_title from " . TABLE_FAQDESK_CONFIGURATION_GROUP . " where configuration_group_id = '" . $_GET['gID'] . "'"
-);
+          while ($configuration = tep_db_fetch_array($configuration_query))
+              tep_db_query("UPDATE ".TABLE_FAQDESK_CONFIGURATION." SET configuration_value='".$_POST[$configuration['configuration_key']]."' where configuration_key='".$configuration['configuration_key']."'");
 
-$cfg_group = tep_db_fetch_array($cfg_group_query);
+               tep_redirect(FILENAME_FAQDESK_CONFIGURATION. '?gID=' . (int)$_GET['gID']);
+        break;
+
+    }
+  }
+
+  $cfg_group_query = tep_db_query("select configuration_group_title, configuration_group_key from " . TABLE_FAQDESK_CONFIGURATION_GROUP . " where configuration_group_id = '" . (int)$_GET['gID'] . "'");
+  $cfg_group = tep_db_fetch_array($cfg_group_query);
 ?>
 
 <!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
@@ -65,147 +64,69 @@ $cfg_group = tep_db_fetch_array($cfg_group_query);
 </table>
 		</td>
 	</tr>
-	<tr>
-		<td>
-<table border="0" width="100%" cellspacing="0" cellpadding="0">
-	<tr>
-		<td valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
-	<tr class="dataTableHeadingRow">
-		<td class="dataTableHeadingContent"><?php echo TABLE_HEADING_CONFIGURATION_TITLE; ?></td>
-		<td class="dataTableHeadingContent"><?php echo TABLE_HEADING_CONFIGURATION_VALUE; ?></td>
-		<td class="dataTableHeadingContent" align="right"><?php echo TABLE_HEADING_ACTION; ?>&nbsp;</td>
-	</tr>
 
+<?php echo tep_draw_form('configuration', FILENAME_FAQDESK_CONFIGURATION, 'gID=' . (int)$_GET['gID'] . '&action=save'); ?>
+            <table width="100%"  border="0" cellspacing="0" cellpadding="4">
 <?php
- 
- $configuration_query = tep_db_query(
- "select configuration_id, configuration_title, configuration_key, configuration_value, use_function from " . TABLE_FAQDESK_CONFIGURATION . " where configuration_group_id = '" . $_GET['gID'] . "' order by sort_order"
-);
+  $configuration_query = tep_db_query("select configuration_key,configuration_id, configuration_value, use_function,set_function from " . TABLE_FAQDESK_CONFIGURATION . " where configuration_group_id = '" . (int)$_GET['gID'] . "' order by sort_order");
 
-while ($configuration = tep_db_fetch_array($configuration_query)) {
-	if (tep_not_null($configuration['use_function'])) {
-		$use_function = $configuration['use_function'];
+  while ($configuration = tep_db_fetch_array($configuration_query)) {
+
+    if (tep_not_null($configuration['use_function'])) {
+      $use_function = $configuration['use_function'];
 		if (preg_match('/->/', $use_function)) {
-			$class_method = explode('->', $use_function);
-			if (!is_object(${$class_method[0]})) {
-				include(DIR_WS_CLASSES . $class_method[0] . '.php');
-				${$class_method[0]} = new $class_method[0]();
-			}
-		$cfgValue = tep_call_function($class_method[1], $configuration['configuration_value'], ${$class_method[0]});
-		} else {
-			$cfgValue = tep_call_function($use_function, $configuration['configuration_value']);
-		}
-	} else {
-		$cfgValue = $configuration['configuration_value'];
-	}
+        $class_method = explode('->', $use_function);
+        if (!is_object(${$class_method[0]})) {
+          include(DIR_WS_CLASSES . $class_method[0] . '.php');
+          ${$class_method[0]} = new $class_method[0]();
+        }
+        $cfgValue = tep_call_function($class_method[1], $configuration['configuration_value'], ${$class_method[0]});
+      } else {
+        $cfgValue = tep_call_function($use_function, $configuration['configuration_value']);
+      }
+    } else {
+      $cfgValue = $configuration['configuration_value'];
+    }
 
-	if (((!$_GET['cID']) || (@$_GET['cID'] == $configuration['configuration_id'])) && (!$cInfo) && (substr($_GET['action'], 0, 3) != 'new')) {
+    if (((!$_GET['cID']) || (@$_GET['cID'] == $configuration['configuration_id'])) && (!$cInfo) && (substr($_GET['action'], 0, 3) != 'new')) {
+      $cfg_extra_query = tep_db_query("select configuration_key,configuration_value, date_added, last_modified, use_function, set_function from " . TABLE_FAQDESK_CONFIGURATION . " where configuration_id = '" . $configuration['configuration_id'] . "'");
+      $cfg_extra = tep_db_fetch_array($cfg_extra_query);
 
-		$cfg_extra_query = tep_db_query(
-"select configuration_key, configuration_description, date_added, last_modified, use_function, set_function from " . TABLE_FAQDESK_CONFIGURATION . " where configuration_id = '" . $configuration['configuration_id'] . "'"
-);
+      $cInfo_array = tep_array_merge($configuration, $cfg_extra);
+      $cInfo = new objectInfo($cInfo_array);
+    }
+    if ($configuration['set_function']) {
+        eval('$value_field = ' . $configuration['set_function'] . '"' . htmlspecialchars($configuration['configuration_value']) . '");');
+      } else {
+        $value_field = tep_draw_input_field($configuration['configuration_key'], $configuration['configuration_value'],'size=40');
+      }
+   // add
 
-		$cfg_extra = tep_db_fetch_array($cfg_extra_query);
+   if (strstr($value_field,'configuration_value')) $value_field=str_replace('configuration_value',$configuration['configuration_key'],$value_field);
 
-		$cInfo_array = array_merge($configuration, $cfg_extra);
-		$cInfo = new objectInfo($cInfo_array);
-	}
+   echo '
+  <tr>
+    <td width="300" valign="top" class="dataTableContent"><b>'.constant(strtoupper($configuration['configuration_key'].'_TITLE')).'</b></td>
+    <td valign="top" class="dataTableContent">
+    <table width="100%"  border="0" cellspacing="0" cellpadding="2">
+      <tr>
+        <td style="background-color:#DFDFDF ; border: 1px solid; border-color: #CCCCCC;" class="dataTableContent">'.$value_field.'</td>
+      </tr>
+    </table>
+    <br />'.constant(strtoupper( $configuration['configuration_key'].'_DESC')).'</td>
+  </tr>
+  ';
 
-	if ( (is_object($cInfo)) && ($configuration['configuration_id'] == $cInfo->configuration_id) ) {
-		echo '<tr class="dataTableRowSelected" onmouseover="this.style.cursor=\'hand\'" onclick="document.location.href=\'' 
-		. tep_href_link(FILENAME_FAQDESK_CONFIGURATION, 'gID=' . $_GET['gID'] . '&cID=' . $cInfo->configuration_id . '&action=edit') 
-		. '\'">' . "\n";
-	} else {
-		echo '<tr class="dataTableRow" onmouseover="this.className=\'dataTableRowOver\';this.style.cursor=\'hand\'" 
-		onmouseout="this.className=\'dataTableRow\'" onclick="document.location.href=\'' . tep_href_link(FILENAME_FAQDESK_CONFIGURATION, 
-		'gID=' . $_GET['gID'] . '&cID=' . $configuration['configuration_id']) . '\'">' . "\n";
-	}
+  }
 ?>
+            </table>
+<?php echo tep_image_submit('button_update.gif', IMAGE_UPDATE); ?></form>
+            </td>
 
-		<td class="dataTableContent"><?php echo constant(strtoupper( $configuration['configuration_key'].'_TITLE')); ?></td>
-		<td class="dataTableContent"><?php echo htmlspecialchars($cfgValue); ?></td>
-		<td class="dataTableContent" align="right">
-<?php
-if ( (is_object($cInfo)) && ($configuration['configuration_id'] == $cInfo->configuration_id) ) {
-	echo tep_image(DIR_WS_IMAGES . 'icon_arrow_right.gif', '');
-} else {
-	echo '<a href="' . tep_href_link(FILENAME_FAQDESK_CONFIGURATION, 'gID=' . $_GET['gID'] . '&cID=' 
-	. $configuration['configuration_id']) . '">' . tep_image(DIR_WS_IMAGES . 'icon_info.gif', IMAGE_ICON_INFO) . '</a>';
-}
-?>
-		&nbsp;</td>
-	</tr>
-
-<?php
-}
-?>
-
-</table>
-		</td>
-
-<?php
-$heading = array();
-$contents = array();
-switch ($_GET['action']) {
-case 'edit':
-	$heading[] = array('text' => '<b>' . constant(strtoupper($cInfo->configuration_key .'_TITLE')) . '</b>');
-
-	if ($cInfo->set_function) {
-		eval('$value_field = ' . $cInfo->set_function . '"' . htmlspecialchars($cInfo->configuration_value) . '");');
-	} else {
-			$value_field = tep_draw_input_field('configuration_value', $cInfo->configuration_value);
-	}
-
-	$contents = array(
-	'form' => tep_draw_form('configuration', FILENAME_FAQDESK_CONFIGURATION, 'gID=' . $_GET['gID'] . '&cID=' . $cInfo->configuration_id 
-	. '&action=save')
-	);
-
-	$contents[] = array('text' => TEXT_INFO_EDIT_INTRO);
-   $contents[] = array('text' => '<br><b>' . constant(strtoupper($cInfo->configuration_key .'_TITLE')) . '</b><br>' . constant(strtoupper($cInfo->configuration_key .'_DESC')) . '<br>' . $value_field);
-//	$contents[] = array('text' => '<br><b>' . $cInfo->configuration_title . '</b><br>' . $cInfo->configuration_description . '<br>' . $value_field);
-	$contents[] = array(
-		'align' => 'center',
-		'text' => '<br>' . tep_image_submit('button_update.gif', IMAGE_UPDATE) . '&nbsp;<a href="' 
-		. tep_href_link(FILENAME_FAQDESK_CONFIGURATION, 'gID=' . $_GET['gID'] . '&cID=' . $cInfo->configuration_id) 
-		. '">' . tep_image_button('button_cancel.gif', IMAGE_CANCEL) . '</a>'
-		);
-	break;
-
-default:
-if (is_object($cInfo)) {
-	$heading[] = array('text' => '<b>' . constant(strtoupper($cInfo->configuration_key .'_TITLE')) . '</b>');
-
-	$contents[] = array(
-		'align' => 'center',
-		'text' => '<a href="' . tep_href_link(FILENAME_FAQDESK_CONFIGURATION, 'gID=' . $_GET['gID'] 
-		. '&cID=' . $cInfo->configuration_id . '&action=edit') . '">' . tep_image_button('button_edit.gif', IMAGE_EDIT) . '</a>'
-		);
-
-	$contents[] = array('text' => '<br>' . constant(strtoupper($cInfo->configuration_key .'_DESC')));
-	$contents[] = array('text' => '<br>' . TEXT_INFO_DATE_ADDED . ' ' . tep_date_short($cInfo->date_added));
-	if (tep_not_null($cInfo->last_modified)) $contents[] = array('text' => TEXT_INFO_LAST_MODIFIED . ' ' . tep_date_short($cInfo->last_modified));
-	}
-	break;
-}
-
-if ( (tep_not_null($heading)) && (tep_not_null($contents)) ) {
-	echo '<td width="25%" valign="top">' . "\n";
-
-	$box = new box;
-	echo $box->infoBox($heading, $contents);
-
-	echo '</td>' . "\n";
-
-}
-?>
-
-	</tr>
-</table>
-		</td>
-	</tr>
-</table>
-		</td>
+          </tr>
+        </table></td>
+      </tr>
+    </table></td>
 <!-- body_text_eof //-->
 	</tr>
 </table>
