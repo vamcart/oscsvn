e9a4493afb3c2aa1287457d971a70184fe4e741d
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2010-05-30 07:54:00 +0000

    Корректировка страницы настроек в админке

--- bts/admin/configuration.php
+++ bts/admin/configuration.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: configuration.php,v 1.2 2003/09/24 13:57:05 wilt Exp $
+  $Id: configuration.php,v 1.3 2003/09/24 13:57:05 wilt Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
