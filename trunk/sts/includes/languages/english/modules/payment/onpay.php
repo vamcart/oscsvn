3ba4a607ad85073220a34f5776a65dffc9ede054
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2010-06-02 09:40:25 +0000

    Добавлен новый модуль оплаты - OnPay

---
+++ trunk/sts/includes/languages/english/modules/payment/onpay.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  $Id: onpay.php 1778 2008-01-09 23:37:44Z hpdl $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2008 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_ONPAY_TEXT_TITLE', 'OnPay');
+  define('MODULE_PAYMENT_ONPAY_TEXT_PUBLIC_TITLE', 'OnPay');
+  define('MODULE_PAYMENT_ONPAY_TEXT_DESCRIPTION', 'После нажатия кнопки Подтвердить заказ Вы перейдёте на сайт платёжной системы для оплаты заказа, после оплаты Ваш заказ будет выполнен.');
+  define('MODULE_PAYMENT_ONPAY_TEXT_ADMIN_DESCRIPTION', 'Модуль оплаты OnPay<br />Как правильно настроить модуль читайте <a href="http://oscommerce.su/modules/smartsection/item.php?itemid=15" target="_blank"><u>здесь</u></a>.');
+  
+?>
