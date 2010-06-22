79f7e69e896cc404b4f3172c0e7296e161bb8acc
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2010-06-22 06:50:43 +0000

    Добавлен новый модуль оплату - PayOnlineSystem

---
+++ bts/includes/languages/english/modules/payment/payonlinesystem.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  $Id: payonlinesystem.php 1778 2010-06-22 23:37:44Z oleg_vamsoft $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 VaMSoft Ltd. http://kypi.ru
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_PAYONLINESYSTEM_TEXT_TITLE', 'PayOnline System (Visa, Mastercard)');
+  define('MODULE_PAYMENT_PAYONLINESYSTEM_TEXT_PUBLIC_TITLE', 'PayOnline System (Visa, Mastercard)');
+  define('MODULE_PAYMENT_PAYONLINESYSTEM_TEXT_DESCRIPTION', 'После нажатия кнопки Подтвердить заказ Вы перейдёте на сайт платёжной системы для оплаты заказа, после оплаты Ваш заказ будет выполнен.');
+  define('MODULE_PAYMENT_PAYONLINESYSTEM_TEXT_ADMIN_DESCRIPTION', 'Модуль оплаты PayOnline System (Visa, Mastercard)<br />Как правильно настроить модуль читайте <a href="http://oscommerce.su/modules/smartsection/item.php?itemid=16" target="_blank"><u>здесь</u></a>.');
+
+?>
