13dfdf6cc7e9e8f0af26940b59f561b74883fb38
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2010-07-14 09:15:27 +0000

    Добавлен новый модуль оплаты - ActivePay

---
+++ trunk/bts/includes/languages/russian/modules/payment/broadcash.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  Payment module translation for Broadcash payment system
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_BROADCASH_TEXT_TITLE', 'Broadcash');
+  define('MODULE_PAYMENT_BROADCASH_TEXT_PUBLIC_TITLE', 'Broadcash');
+  define('MODULE_PAYMENT_BROADCASH_TEXT_DESCRIPTION', 'Broadcash Info:<br><br>Site: <a href="http://activepay.ru">Activepay</a>');
+  define('MODULE_PAYMENT_BROADCASH_TEXT_ERROR', 'Broadcash payment error!');
+?>
