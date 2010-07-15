d8b42264e282cab9c64a99076614f73adf1eeced
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2010-07-15 09:51:43 +0000

    Корректировка кода вывода товаров внутри категорий, спасибо CDesign

--- trunk/sts/includes/modules/product_listing_col.php
+++ trunk/sts/includes/modules/product_listing_col.php
@@ -119,9 +119,20 @@ if ($listing_split->number_of_pages > 1) {
         $lc_text = tep_create_sort_heading($_GET['sort'], $col+1, $lc_text);
       }
 
+/* ORIGINAL 213
       $list_box_contents[0][] = array('align' => $lc_align,
                                      'params' => 'class="productListing-heading"',
                                        'text' => '&nbsp;' . $lc_text . '&nbsp;');
+*/
+/* CDS Patch. 4. BOF */
+          if ($list_box_contents[0][$column_list_order[$col]]['text'] != '')
+          {
+            $lc_text = $list_box_contents[0][$column_list_order[$col]]['text'] . ' / ' . $lc_text;
+          }
+          $list_box_contents[0][$column_list_order[$col]] = array('align' => $lc_align,
+                                                'params' => 'class="productListing-heading"',
+                                                 'text'  => $lc_text == ''?'&nbsp;':$lc_text);
+/* CDS Patch. 4. EOF */
     }
 
     if ($listing_split->number_of_rows > 0) {
@@ -252,9 +263,20 @@ if (tep_has_product_attributes($listing['products_id'])) {
               break;
           }
 
+/* ORIGINAL 213
           $list_box_contents[$cur_row][] = array('align' => $lc_align,
                                                 'params' => 'class="productListing-data"',
                                                  'text'  => $lc_text);
+*/
+/* CDS Patch. 4. BOF */
+          if ($list_box_contents[$cur_row][$column_list_order[$col]]['text'] != '')
+          {
+            $lc_text = $list_box_contents[$cur_row][$column_list_order[$col]]['text'] . '<br />' . $lc_text;
+          }
+          $list_box_contents[$cur_row][$column_list_order[$col]] = array('align' => $lc_align,
+                                                'params' => 'class="productListing-data"',
+                                                 'text'  => $lc_text);
+/* CDS Patch. 4. EOF */
         }
       }
 
