5a6a3f7c6ac3a847d964ea6bc5e830f1b92fd0a4
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2010-10-21 07:48:28 +0000

    Корректировка кода модулей оплаты, добавлена запись комментария

--- bts/includes/modules/payment/qiwi.php
+++ bts/includes/modules/payment/qiwi.php
@@ -194,6 +194,10 @@
 
           $insert_id = tep_db_insert_id();
 
+			$customer_notification = (SEND_EMAILS == 'true') ? '1' : '0';
+			$sql_data_array = array ('orders_id' => $insert_id, 'orders_status_id' => $order->info['order_status'], 'date_added' => 'now()', 'customer_notified' => $customer_notification, 'comments' => $order->info['comments']);
+			tep_db_perform(TABLE_ORDERS_STATUS_HISTORY, $sql_data_array);
+
           for ($i=0, $n=sizeof($order_totals); $i<$n; $i++) {
             $sql_data_array = array('orders_id' => $insert_id,
                                     'title' => $order_totals[$i]['title'],
