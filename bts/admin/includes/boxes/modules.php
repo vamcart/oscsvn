96b521527090bd76597c8e8f6c7d54a7aed96001
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2010-12-14 13:55:02 +0000

    Перекодировка файлов в utf8

--- bts/admin/includes/boxes/modules.php
+++ bts/admin/includes/boxes/modules.php
@@ -1,36 +1,43 @@
 <?php
 /*
-  $Id: modules.php,v 1.2 2003/09/24 13:57:07 wilt Exp $
+  $Id$
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2002 osCommerce
+  Copyright (c) 2010 osCommerce
 
   Released under the GNU General Public License
 */
+
+  $cl_box_groups[] = array(
+    'heading' => BOX_HEADING_MODULES,
+    'apps' => array(
+      array(
+        'code' => FILENAME_MODULES,
+        'title' => BOX_MODULES_PAYMENT,
+        'link' => tep_href_link(FILENAME_MODULES, 'set=payment', 'NONSSL')
+      ),
+      array(
+        'code' => FILENAME_MODULES,
+        'title' => BOX_MODULES_SHIPPING,
+        'link' => tep_href_link(FILENAME_MODULES, 'set=shipping', 'NONSSL')
+      ),
+      array(
+        'code' => FILENAME_MODULES,
+        'title' => BOX_MODULES_ORDER_TOTAL,
+        'link' => tep_href_link(FILENAME_MODULES, 'set=ordertotal', 'NONSSL')
+      ),
+      array(
+        'code' => FILENAME_CIP_MANAGER,
+        'title' => CIP_TITLE,
+        'link' => tep_href_link(FILENAME_CIP_MANAGER)
+      ),
+      array(
+        'code' => FILENAME_SHIP2PAY,
+        'title' => BOX_MODULES_SHIP2PAY,
+        'link' => tep_href_link(FILENAME_SHIP2PAY)
+      )
+    )
+  );
 ?>
-<!-- modules //-->
-          <tr>
-            <td>
-<?php
-  $heading = array();
-  $contents = array();
-
-  $heading[] = array('text'  => BOX_HEADING_MODULES,
-                     'link'  => tep_href_link(FILENAME_MODULES, 'set=payment&selected_box=modules'));
-
-  if ($selected_box == 'modules' || $menu_dhtml == true) {
-    $contents[] = array('text'  => '<a href="' . tep_href_link(FILENAME_MODULES, 'set=payment', 'NONSSL') . '" class="menuBoxContentLink">' . BOX_MODULES_PAYMENT . '</a><br>' .
-                                   '<a href="' . tep_href_link(FILENAME_MODULES, 'set=shipping', 'NONSSL') . '" class="menuBoxContentLink">' . BOX_MODULES_SHIPPING . '</a><br>' .
-                                   '<a href="' . tep_href_link(FILENAME_MODULES, 'set=ordertotal', 'NONSSL') . '" class="menuBoxContentLink">' . BOX_MODULES_ORDER_TOTAL . '</a><br>' . 
-                                   '<a href="' . tep_href_link(FILENAME_CIP_MANAGER, 'selected_box=modules', 'NONSSL') . '" class="menuBoxContentLink">' . CIP_TITLE . '</a><br>' . 
-                                   '<a href="' . tep_href_link(FILENAME_SHIP2PAY, 'set=ordertotal', 'NONSSL') . '" class="menuBoxContentLink">' . BOX_MODULES_SHIP2PAY . '</a>');
-  }
-
-  $box = new box;
-  echo $box->menuBox($heading, $contents);
-?>
-            </td>
-          </tr>
-<!-- modules_eof //-->
