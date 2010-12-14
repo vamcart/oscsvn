0dc3a9d5456e6a2489abf3b8cc4a71afd4341b4d
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2010-12-14 13:55:02 +0000

    Перекодировка файлов в utf8

--- trunk/sts/admin/includes/boxes/customers.php
+++ trunk/sts/admin/includes/boxes/customers.php
@@ -1,44 +1,43 @@
 <?php
 /*
-  $Id: customers.php,v 1.2 2003/09/24 13:57:07 wilt Exp $
+  $Id$
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2002 osCommerce
+  Copyright (c) 2010 osCommerce
 
   Released under the GNU General Public License
 */
+
+  $cl_box_groups[] = array(
+    'heading' => BOX_HEADING_CUSTOMERS,
+    'apps' => array(
+      array(
+        'code' => FILENAME_CUSTOMERS,
+        'title' => BOX_CUSTOMERS_CUSTOMERS,
+        'link' => tep_href_link(FILENAME_CUSTOMERS)
+      ),
+      array(
+        'code' => FILENAME_ORDERS,
+        'title' => BOX_CUSTOMERS_ORDERS,
+        'link' => tep_href_link(FILENAME_ORDERS)
+      ),
+      array(
+        'code' => FILENAME_GROUPS,
+        'title' => BOX_CUSTOMERS_GROUPS,
+        'link' => tep_href_link(FILENAME_GROUPS)
+      ),
+      array(
+        'code' => FILENAME_CREATE_ACCOUNT,
+        'title' => BOX_MANUAL_ORDER_CREATE_ACCOUNT,
+        'link' => tep_href_link(FILENAME_CREATE_ACCOUNT)
+      ),
+      array(
+        'code' => FILENAME_CREATE_ORDER,
+        'title' => BOX_MANUAL_ORDER_CREATE_ORDER,
+        'link' => tep_href_link(FILENAME_CREATE_ORDER)
+      )
+    )
+  );
 ?>
-<!-- customers //-->
-          <tr>
-            <td>
-<?php
-  $heading = array();
-  $contents = array();
-
-  $heading[] = array('text'  => BOX_HEADING_CUSTOMERS,
-                     'link'  => tep_href_link(FILENAME_CUSTOMERS, 'selected_box=customers'));
-
-  if ($selected_box == 'customers' || $menu_dhtml == true) {
-    $contents[] = array('text'  =>
-//Admin begin
-
-                                   tep_admin_files_boxes(FILENAME_CUSTOMERS, BOX_CUSTOMERS_CUSTOMERS) .
-                                   tep_admin_files_boxes(FILENAME_ORDERS, BOX_CUSTOMERS_ORDERS) .
-                                   tep_admin_files_boxes(FILENAME_GROUPS, BOX_CUSTOMERS_GROUPS) .
-//                                   tep_admin_files_boxes(FILENAME_MANUAL_DISCOUNTS, BOX_MANUDISCOUNT) .
-                                   tep_admin_files_boxes(FILENAME_CREATE_ACCOUNT, BOX_MANUAL_ORDER_CREATE_ACCOUNT) .
-                                   tep_admin_files_boxes(FILENAME_CREATE_ORDER, BOX_MANUAL_ORDER_CREATE_ORDER));
-                                   
-                                 
-                                   
-//Admin end
-  }
-
-  $box = new box;
-  echo $box->menuBox($heading, $contents);
-?>
-            </td>
-          </tr>
-<!-- customers_eof //-->
