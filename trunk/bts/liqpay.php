6f394e4fb2b1cd04543a3ae5d4930eed46be1fd7
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2011-01-04 18:59:32 +0000

    Корректировка кода в модуле оплаты LiqPAY

--- trunk/bts/liqpay.php
+++ trunk/bts/liqpay.php
@@ -30,7 +30,7 @@ require (DIR_WS_CLASSES.'order.php');
 
 // variables prepearing
 
-$xml_decoded=decode_base64($_POST['xml']);
+$xml_decoded=base64_decode($_POST['xml']);
 
 $xml = simplexml_load_string($xml_decoded);
 
