c0b199792bfb71c8a8683b05b8c76fe69ca17306
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2011-02-14 10:18:59 +0000

    Корректировка кода в модуле оплаты единая касса

--- sts/wallet_one.php
+++ sts/wallet_one.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: webmoney.php 1778 2008-01-09 23:37:44Z hpdl $
+  $Id: wallet_one.php 1778 2008-01-09 23:37:44Z hpdl $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -27,30 +27,24 @@ require (DIR_WS_CLASSES.'order.php');
 //fwrite($fp, $str."\n");
 //fclose($fp);
 // variables prepearing
-$crc = get_var('LMI_HASH');
 
-$inv_id = get_var('LMI_PAYMENT_NO');
+$inv_id = get_var('WMI_PAYMENT_NO');
 $order = new order($inv_id);
-$order_sum = $order->info['total'];
-
-$hash = strtoupper(md5($_POST['LMI_PAYEE_PURSE'].$_POST['LMI_PAYMENT_AMOUNT'].$_POST['LMI_PAYMENT_NO'].$_POST['LMI_MODE']. 
-$_POST['LMI_SYS_INVS_NO'].$_POST['LMI_SYS_TRANS_NO'].$_POST['LMI_SYS_TRANS_DATE'].MODULE_PAYMENT_WEBMONEY_MERCHANT_SECRET_KEY. 
-$_POST['LMI_PAYER_PURSE'].$_POST['LMI_PAYER_WM'])); 
 
 // checking and handling
-if ($hash == $crc) {
-if (number_format($_POST['LMI_PAYMENT_AMOUNT'],0) == number_format($order->info['total'],0)) {
-  $sql_data_array = array('orders_status' => MODULE_PAYMENT_WEBMONEY_MERCHANT_ORDER_STATUS_ID);
+if ($_POST['WMI_ORDER_STATE'] == 'Accepted') {
+if (number_format($_POST['WMI_PAYMENT_AMOUNT'],2) == number_format($order->info['total'],2)) {
+  $sql_data_array = array('orders_status' => MODULE_PAYMENT_WALLET_ONE_ORDER_STATUS_ID);
   tep_db_perform('orders', $sql_data_array, 'update', "orders_id='".$inv_id."'");
 
   $sql_data_arrax = array('orders_id' => $inv_id,
-                          'orders_status_id' => MODULE_PAYMENT_WEBMONEY_MERCHANT_ORDER_STATUS_ID,
+                          'orders_status_id' => MODULE_PAYMENT_WALLET_ONE_ORDER_STATUS_ID,
                           'date_added' => 'now()',
                           'customer_notified' => '0',
-                          'comments' => 'WebMoney accepted this order payment');
+                          'comments' => 'W1 accepted this order payment');
   tep_db_perform('orders_status_history', $sql_data_arrax);
 
-  echo 'OK'.$inv_id;
+  echo 'WMI_RESULT=OK';
 }
 }
 ?>
