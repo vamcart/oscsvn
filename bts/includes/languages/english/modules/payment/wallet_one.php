e14118ef7cceeedbd9680b72f17bf02cfd9962e3
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2011-02-14 10:55:38 +0000

    Корректировка кода в модуле оплаты Единая Касса

--- bts/includes/languages/english/modules/payment/wallet_one.php
+++ bts/includes/languages/english/modules/payment/wallet_one.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: webmoney_merchant.php 1778 2008-01-09 23:37:44Z hpdl $
+  $Id: wallet_one.php 1778 2008-01-09 23:37:44Z hpdl $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -10,9 +10,9 @@
   Released under the GNU General Public License
 */
 
-  define('MODULE_PAYMENT_WEBMONEY_MERCHANT_TEXT_TITLE', 'WebMoney');
-  define('MODULE_PAYMENT_WEBMONEY_MERCHANT_TEXT_PUBLIC_TITLE', 'WebMoney Merchant');
-  define('MODULE_PAYMENT_WEBMONEY_MERCHANT_TEXT_DESCRIPTION', 'После нажатия кнопки Подтвердить заказ Вы перейдёте на сайт платёжной системы для оплаты заказа, после оплаты Ваш заказ будет выполнен.');
-  define('MODULE_PAYMENT_WEBMONEY_MERCHANT_TEXT_ADMIN_DESCRIPTION', 'Модуль оплаты WebMoney Merchant<br />Как правильно настроить модуль читайте <a href="http://oscommerce.su/modules/smartsection/item.php?itemid=10" target="_blank"><u>здесь</u></a>.');
-  
+  define('MODULE_PAYMENT_WALLET_ONE_TEXT_TITLE', 'Единая касса');
+  define('MODULE_PAYMENT_WALLET_ONE_TEXT_PUBLIC_TITLE', 'Единая касса');
+  define('MODULE_PAYMENT_WALLET_ONE_TEXT_DESCRIPTION', 'После нажатия кнопки Подтвердить заказ Вы перейдёте на сайт платёжной системы для оплаты заказа, после оплаты Ваш заказ будет выполнен.');
+  define('MODULE_PAYMENT_WALLET_ONE_TEXT_ADMIN_DESCRIPTION', 'Модуль оплаты Единая касса (w1.ru)<br />Как правильно настроить модуль читайте <a href="http://oscommerce.su/modules/smartsection/item.php?itemid=18" target="_blank"><u>здесь</u></a>.');
+
 ?>
