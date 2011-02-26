095a194150b14a3f7418439ad0bfa2f77787e9d8
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2011-02-26 16:54:14 +0000

    Добавлена возможность экспорта клиентов и заказов в файл

---
+++ trunk/bts/admin/includes/languages/russian/customer_export.php
@@ -0,0 +1,32 @@
+<?php
+/*
+  $Id: newsletters.php,v 1.5 2002/03/08 22:10:08 hpdl Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Выгрузка клиентов');
+define('TABLE_HEADING_CUSTOMER_EXPORT', 'Выгрузка и сохранение клиентской базы на локальный компьютер');
+define('TEXT_CUSTOMER_EXPORT_ALL','Все записи по клиентам будут сохранены в один файл');
+define('TEXT_CUSTOMER_EXPORT','Выгрузка');
+define('TEXT_CUSTOMER_EXPORT_SEPARATOR','Разделение');
+define('TABLE_HEADING_FIRSTNAME', 'Имя');
+define('TABLE_HEADING_LASTNAME', 'Фамилия');
+define('TABLE_HEADING_EMAIL', 'Email:');
+define('TABLE_HEADING_GENDER', 'Пол');
+define('TABLE_HEADING_LOCATION', 'Регион');
+define('TABLE_HEADING_TELEPHONE', 'Телефон');
+define('TABLE_HEADING_DOB', 'Д.Р.');
+define('TABLE_HEADING_COUNTRY', 'Страна');
+define('TABLE_HEADING_COMPANY', 'Компания');
+define('TABLE_HEADING_STREET', 'Адрес');
+define('TABLE_HEADING_ZIP', 'Индекс');
+define('TABLE_HEADING_CITY', 'Город');
+define('TABLE_HEADING_SUBURB', 'Район');
+define('TABLE_HEADING_FAX', 'Факс');
+?>
---
+++ trunk/bts/admin/includes/languages/russian/exportorders.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $Id: customers.php,v 1.12 2002/01/12 18:46:27 hpdl Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Выгрузка заказов в excel');
+
+define('INPUT_START', 'От номера');
+define('INPUT_END', 'До номера');
+define('INPUT_VALID', 'Сохранить в .xls');
+define('INPUT_DESC', 'Выберите номера заказов для экспорта. Чтобы сохранить ВСЕ заказы оставьте оба поля пустыми. Оставьте одно поле пустым, чтобы сохранить заказы до- или после- заказа такого-то.');
+?>
