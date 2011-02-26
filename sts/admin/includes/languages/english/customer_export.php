68cf18f09f0bad003b1afa3cb68850ba5252f4c8
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2011-02-26 16:54:14 +0000

    Добавлена возможность экспорта клиентов и заказов в файл

---
+++ sts/admin/includes/languages/english/customer_export.php
@@ -0,0 +1,32 @@
+<?php
+/*
+  $Id: newsletters.php,v 1.5 2002/03/08 22:10:08 hpdl Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Customers export');
+define('TABLE_HEADING_CUSTOMER_EXPORT', 'Customers export');
+define('TEXT_CUSTOMER_EXPORT_ALL','Export all');
+define('TEXT_CUSTOMER_EXPORT','Export');
+define('TEXT_CUSTOMER_EXPORT_SEPARATOR','Separator');
+define('TABLE_HEADING_FIRSTNAME', 'First name');
+define('TABLE_HEADING_LASTNAME', 'Last name');
+define('TABLE_HEADING_EMAIL', 'Email:');
+define('TABLE_HEADING_GENDER', 'Gender');
+define('TABLE_HEADING_LOCATION', 'Location');
+define('TABLE_HEADING_TELEPHONE', 'Telephone');
+define('TABLE_HEADING_DOB', 'Date of birth');
+define('TABLE_HEADING_COUNTRY', 'Country');
+define('TABLE_HEADING_COMPANY', 'Company');
+define('TABLE_HEADING_STREET', 'Street Address');
+define('TABLE_HEADING_ZIP', 'Postcode');
+define('TABLE_HEADING_CITY', 'City');
+define('TABLE_HEADING_SUBURB', 'Suburb');
+define('TABLE_HEADING_FAX', 'Fax');
+?>
---
+++ sts/admin/includes/languages/english/exportorders.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $Id: customers.php,v 1.12 2002/01/12 18:46:27 hpdl Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Orders export');
+
+define('INPUT_START', 'From #');
+define('INPUT_END', 'To');
+define('INPUT_VALID', 'Save to .xls');
+define('INPUT_DESC', '');
+?>
