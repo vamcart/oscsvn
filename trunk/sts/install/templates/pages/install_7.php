2f95d1291da27112a232d28fd8e102da53fccc13
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2011-04-11 08:14:56 +0000

    Корректировка скрипта установки

--- trunk/sts/install/templates/pages/install_7.php
+++ trunk/sts/install/templates/pages/install_7.php
@@ -262,7 +262,8 @@
                      '  define(\'OSC_COOKIE_DOMAIN\', \'' . $http_cookie_domain . '\');' . "\n" .
                      '  define(\'OSC_COOKIE_PATH\', \'/\');' . "\n" .
                      '' . "\n" .
-
+                     '  define(\'SESSION_WRITE_DIRECTORY\', DIR_FS_CATALOG . \'temp/\');' . "\n" .
+                     '' . "\n" .
                      '// Added for Templating' . "\n" .
   		     '	define(\'DIR_FS_CATALOG_MAINPAGE_MODULES\', DIR_FS_CATALOG_MODULES . \'mainpage_modules/\');' . "\n" .
   		     '	define(\'DIR_WS_TEMPLATES\', DIR_WS_CATALOG . \'templates/\');' . "\n" .
