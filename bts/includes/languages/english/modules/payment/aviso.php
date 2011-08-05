5a74a25fba1aed7e567bf485f43ff48609c64b9d
oscvam <oscvam@35220d00-f9cb-11de-91aa-85f76f42a988>
2011-08-05 14:02:43 +0000

    Корректировка перевода

--- bts/includes/languages/english/modules/payment/aviso.php
+++ bts/includes/languages/english/modules/payment/aviso.php
@@ -10,16 +10,16 @@
   Released under the GNU General Public License
 */
 
-  define('MODULE_PAYMENT_AVISO_TEXT_TITLE', 'Киви');
-  define('MODULE_PAYMENT_AVISO_TEXT_PUBLIC_TITLE', 'Киви');
-  define('MODULE_PAYMENT_AVISO_TEXT_DESCRIPTION', 'Для подтверждения заказа нажмите кнопку Подтвердить.<br /><br /><br /><br /><strong><span class="Requirement">Вам был выписан счёт для оплаты заказа в QIWI Кошельке, Вы можете оплатить счёт в любом терминале киви, в своём личном кабинете (киви кошелёк), либо через интернет-версию киви кошелька по адресу <a href="http://mylk.qiwi.ru">http://mylk.qiwi.ru</a></span></strong><br /><br />');
-  define('MODULE_PAYMENT_AVISO_TEXT_ADMIN_DESCRIPTION', 'Модуль оплаты Киви<br />Как правильно настроить модуль читайте <a href="http://oscommerce.su/modules/smartsection/item.php?itemid=14" target="_blank"><u>здесь</u></a>.');
+  define('MODULE_PAYMENT_AVISO_TEXT_TITLE', 'AvisoSMS (Оплата с баланса мобильного телефона)');
+  define('MODULE_PAYMENT_AVISO_TEXT_PUBLIC_TITLE', 'AvisoSMS (Оплата с баланса мобильного телефона)');
+  define('MODULE_PAYMENT_AVISO_TEXT_DESCRIPTION', 'Для подтверждения заказа нажмите кнопку Подтвердить.<br /><br /><br /><br /><strong><span class="Requirement">На указанный номер мобильного телефона ушло SMS сообщение с инструкцией по оплате.</span></strong><br /><br />');
+  define('MODULE_PAYMENT_AVISO_TEXT_ADMIN_DESCRIPTION', 'Модуль оплаты Киви<br />Как правильно настроить модуль читайте <a href="http://oscommerce.su/modules/smartsection/item.php?itemid=19" target="_blank"><u>здесь</u></a>.');
 
 define('MODULE_PAYMENT_AVISO_NAME_TITLE' , '');
 define('MODULE_PAYMENT_AVISO_NAME_DESC' , 'Укажите номер Вашего мобильного телефона.');
 define('MODULE_PAYMENT_AVISO_TELEPHONE' , 'Телефон: ');
 define('MODULE_PAYMENT_AVISO_TELEPHONE_HELP' , ' Пример: <strong>916820XXXX</strong>');
 
-define('MODULE_PAYMENT_AVISO_EMAIL_SUBJECT' , 'КИВИ: Оплачен заказ номер');
+define('MODULE_PAYMENT_AVISO_EMAIL_SUBJECT' , 'AvisoSMS: Оплачен заказ номер');
 
 ?>
