204481d56a734ca8a4621f5f748ab6646e5d8e2c
oscvam <oscvam@35220d00-f9cb-11de-91aa-85f76f42a988>
2011-08-05 13:44:14 +0000

    Добавлен новый модуль оплаты AvisoSMS

---
+++ bts/includes/languages/english/modules/payment/aviso.php
@@ -0,0 +1,25 @@
+<?php
+/*
+  $Id: qiwi.php 106 2010-04-14 13:37:44Z oleg_vamsoft $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 VaMSoft Ltd. http://kypi.ru
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_QIWI_TEXT_TITLE', 'Киви');
+  define('MODULE_PAYMENT_QIWI_TEXT_PUBLIC_TITLE', 'Киви');
+  define('MODULE_PAYMENT_QIWI_TEXT_DESCRIPTION', 'Для подтверждения заказа нажмите кнопку Подтвердить.<br /><br /><br /><br /><strong><span class="Requirement">Вам был выписан счёт для оплаты заказа в QIWI Кошельке, Вы можете оплатить счёт в любом терминале киви, в своём личном кабинете (киви кошелёк), либо через интернет-версию киви кошелька по адресу <a href="http://mylk.qiwi.ru">http://mylk.qiwi.ru</a></span></strong><br /><br />');
+  define('MODULE_PAYMENT_QIWI_TEXT_ADMIN_DESCRIPTION', 'Модуль оплаты Киви<br />Как правильно настроить модуль читайте <a href="http://oscommerce.su/modules/smartsection/item.php?itemid=14" target="_blank"><u>здесь</u></a>.');
+
+define('MODULE_PAYMENT_QIWI_NAME_TITLE' , '');
+define('MODULE_PAYMENT_QIWI_NAME_DESC' , 'Укажите номер Вашего мобильного телефона.');
+define('MODULE_PAYMENT_QIWI_TELEPHONE' , 'Телефон: ');
+define('MODULE_PAYMENT_QIWI_TELEPHONE_HELP' , ' Пример: <strong>916820XXXX</strong>');
+
+define('MODULE_PAYMENT_QIWI_EMAIL_SUBJECT' , 'КИВИ: Оплачен заказ номер');
+
+?>
