2b79108132c2b1f59a11105242cd535b46e8db04
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2012-03-31 05:31:12 +0000

    Корректировка модуля доставки EMS Почта России

--- trunk/bts/includes/modules/shipping/russianpostems.php
+++ trunk/bts/includes/modules/shipping/russianpostems.php
@@ -56,19 +56,20 @@
         if (tep_not_null($this->icon)) $this->quotes['icon'] = tep_image($this->icon, $this->title);
 		
 		
-		//Проверка на максимальный вес	
-$urlWeight = 'http://emspost.ru/api/rest/?method=ems.get.max.weight';		
-	
- // create curl resource
-        $ch = curl_init();
+        $urlCities = "http://emspost.ru/api/rest?method=ems.get.locations&type=cities&plain=true";
+        $urlWeight = "http://emspost.ru/api/rest?method=ems.get.max.weight";
+
+        // create curl resource
+        //$ch = curl_init();
 
         //return the transfer as a string
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        //curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
         // set url
-		curl_setopt($ch, CURLOPT_URL, $urlWeight);
-		$outWeight = curl_exec($ch);
-	
+		//curl_setopt($ch, CURLOPT_URL, $urlWeight);
+		//$outWeight = curl_exec($ch);
+		$outWeight = Curl_Page($urlWeight);
+		
 	    $WeightList = json_decode($outWeight, true);
 
        foreach ($WeightList as $weight){
@@ -83,63 +84,87 @@ $urlWeight = 'http://emspost.ru/api/rest/?method=ems.get.max.weight';
 		
 //Получаем список городов и регионов		
 
-        $urlRussia = 'http://emspost.ru/api/rest?method=ems.get.locations&type=russia';
+        $urlRussia = "http://emspost.ru/api/rest?method=ems.get.locations&type=russia&plain=true";
 
         // create curl resource
-        $ch = curl_init();
+        //$ch = curl_init();
 
         //return the transfer as a string
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        //curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         // set url
-		curl_setopt($ch, CURLOPT_URL, $urlRussia);
-		$outRussia = curl_exec($ch);
-		
-    
+		//curl_setopt($ch, CURLOPT_URL, $urlRussia);
+		//$outRussia = curl_exec($ch);
+		$outRussia = Curl_Page($urlRussia);
+		//print 'outRussia';
+		//print_r($outRussia);
+
 	
 	//Вытягиваем регион магазина
 		$zones_shop = STORE_ZONE;
 		$zones_zones = tep_db_query("select zone_id, zone_name from " . TABLE_ZONES . " where (zone_id='$zones_shop')");
 		$zones_id = tep_db_fetch_array($zones_zones);
 		$zonesshop = $zones_id['zone_name'];
-		
+		//echo $zones_id['zone_name'];
 	//Вытягиваем город получателя
-	$tocity = $order->delivery['city'];	
-	
+	$tocity = $order->delivery['city'];
+	//print $tocity;
 	//Вытягиваем регион получателя 
 	$tostate_id = $order->delivery['state']; 
 	$tostate_tostate = tep_db_query("select zone_id, zone_name from " . TABLE_ZONES . " where (zone_name='$tostate_id')");
+	//print_r($tostate_tostate['zone_id']);
 	$tostate_tostate_id = tep_db_fetch_array($tostate_tostate);
 	$tostate = $tostate_tostate_id['zone_name'];
-	
-	
+	//print($tostate)."<br><br>";
 	
 		
 		//проверяем город/регион отправителя/получателя
-		$RussiaList = json_decode(utf8_encode($outRussia), true);
+		$RussiaList = json_decode($outRussia, true);
         foreach ($RussiaList['rsp']['locations'] as $russia){
-          if (strtolower($russia['name']) == strtolower(MODULE_SHIPPING_RUSSIANPOSTEMS_CITY)){
-            $from = $russia['value'];			
+    		//print mb_convert_case($russia['name'], MB_CASE_UPPER, "UTF-8")."<br>";
+//		print 
+		$str = MODULE_SHIPPING_RUSSIANPOSTEMS_CITY;
+		//print $str;
+		//print "АПП".mb_convert_case($str, MB_CASE_UPPER, "UTF-8");
+		$mode=MB_CASE_UPPER;
+		$conv="UTF-8";
+		$rus01 = mb_convert_case($russia['name'], MB_CASE_UPPER,"UTF-8");
+		$rus02 = mb_convert_case($str, MB_CASE_UPPER,"UTF-8");
+		//print "rus01".$rus02."<br>";
+          if ($rus01 == $rus02){
+            //print "rus01".$rus01."== rus02".$rus02."<br>";
+            //$from = $rus01;
+            $from = $russia['value'];
+            //print "from1 ".$from."<br>";
 		  }
 		  if ($from === null){
-		   if (strtolower($russia['name']) == strtolower($zonesshop)){
-            $from = $russia['value'];			
+		   if (mb_convert_case($russia['name'],$mode,$conv) == mb_convert_case($zonesshop,$mode,$conv)){
+                  //print mb_convert_case($russia['name'],$mode,$conv). "==". mb_convert_case($zonesshop,$mode,$conv)."<br>";
+            
+            $from = $russia['value'];
+            //print "from2 ".$from."<br>";
 		   }
 		  }
 		  
-		  if (strtolower($russia['name']) == strtolower($tocity)){
+		  if (mb_convert_case($russia['name'],$mode,$conv) == mb_convert_case($tocity,$mode,$conv)){
+		    //print mb_convert_case($russia['name'],$mode,$conv) ."==". mb_convert_case($tocity,$mode,$conv)."<br>";
+
             $to = $russia['value'];
-			$tomessag = 'город: '. $russia['name'];
+        		$tomessag = 'город: '. $russia['name'];
+        		//print "To 1".$tomessag;
           }
 		  if ($to === null){
-		   if (strtolower($russia['name']) == strtolower($tostate)){
+		   if (mb_convert_case($russia['name'],$mode,$conv) == mb_convert_case($tostate,$mode,$conv)){
+		    //print mb_convert_case($russia['name'],$mode,$conv) ."==". mb_convert_case($tostate,$mode,$conv)."<br>";
+
             $to = $russia['value'];
 			$tomessag = 'регион: '. $russia['name'];
+			//print "To 2".$tomessag;
 		   }
 		  }
 		 }
-	
-		
+	    //echo "russ list";
+		//print_r($RussiaList);
   // Если вдруг ничего не нашлось		
 		
 	if ($from === null){
@@ -153,16 +178,16 @@ $urlWeight = 'http://emspost.ru/api/rest/?method=ems.get.max.weight';
 
 		
 
-		$url = 'http://emspost.ru/api/rest?method=ems.calculate&from='.$from.'&to='.$to.'&weight='.$shipping_weight;
-		
-		curl_setopt($ch, CURLOPT_URL, $url);
-        $output = curl_exec($ch);
-
+		$url = "http://emspost.ru/api/rest?method=ems.calculate&from=".$from."&to=".$to."&weight=".$shipping_weight;
+		//print "calculaded".$url;
+		//curl_setopt($ch, CURLOPT_URL, $url);
+        //$output = curl_exec($ch);
+	$output = Curl_Page($url);
         // close curl resource to free up system resources
-        curl_close($ch);
+        //curl_close($ch);
 
         $contents = $output;
-        $contents = utf8_encode($contents);
+        $contents = $contents;
         $results = json_decode($contents, true);
 
         if ($results['rsp']['stat'] == 'fail'){
@@ -227,4 +252,87 @@ $urlWeight = 'http://emspost.ru/api/rest/?method=ems.get.max.weight';
       return array('MODULE_SHIPPING_RUSSIANPOSTEMS_STATUS', 'MODULE_SHIPPING_RUSSIANPOSTEMS_CITY', 'MODULE_SHIPPING_RUSSIANPOSTEMS_HANDLING', 'MODULE_SHIPPING_RUSSIANPOSTEMS_TAX_CLASS', 'MODULE_SHIPPING_RUSSIANPOSTEMS_ZONE', 'MODULE_SHIPPING_RUSSIANPOSTEMS_SORT_ORDER', 'MODULE_SHIPPING_RUSSIANPOSTEMS_DCVAL_PERCENT');
     }
   }
+  
+//date_default_timezone_set('Europe/Moscow');
+//date_default_timezone_set('Europe/London');
+
+$agent = "Mozilla/5.1 (Windows; U; Windows NT 5.0; ru-RU; rv:1.7.12) Gecko/20050919 Firefox/1.7.7" ;
+// ставим, что наш броузер читает только файлы в html формате
+$header [] = "Accept: text/html;q=0.9, text/plain;q=0.8, image/png, */*;q=0.5" ;
+$header [] = "Accept_charset: windows-1251, utf-8, utf-16;q=0.6, *;q=0.1";
+// говорим, что броузер не читает файлы в gzip формате
+$header [] = "Accept_encoding: identity";
+$header [] = "Accept_language: en-us,en;q=0.5";
+$header [] = "Connection: close";
+$header [] = "Cache-Control: no-store, no-cache, must-revalidate";
+$header [] = "Keep_alive: 300";
+$header [] = "Expires: Thu, 01 Jan 1970 00:00:01 GMT";
+$cookie_file= "cookie.txt";
+$cookie_file_dop= "cookie_dop_en.txt";
+
+
+
+function data_encode ( $data , $keyprefix = "" , $keypostfix = "" ) {
+    assert ( is_array ( $data ) );
+            $vars = null ;
+            foreach ( $data as $key => $value ) {
+    if ( is_array ( $value )) $vars .= data_encode ( $value , $keyprefix . $key . $keypostfix . urlencode ( "[" ), urlencode ( "]" ));
+        else $vars .= $keyprefix . $key . $keypostfix . "=" . urlencode ( $value ). "&" ;
+                        }
+        return $vars ;
+}
+
+// функция вывода страницы
+
+function Curl_Page ( $path ) {
+        // $path - страничка, какую смотрим
+        global $agent, $header, $referer, $arr_cookie, $cookie_file;
+        // вызываем сеанс Curl
+        $ch = curl_init ( $path );
+        // CURL будет возвращать результат, а не выводить его в печать
+        curl_setopt ( $ch , CURLOPT_RETURNTRANSFER , 1 );
+        // выводим подробные сообщения о всех действиях
+        curl_setopt ( $ch , CURLOPT_VERBOSE , 0 ); //1
+        // считываем страничку с хедером от сервера
+        //curl_setopt ( $ch , CURLOPT_HEADER , 1 );
+        // отправим серверу user_agent сформированный нами самими
+        curl_setopt ( $ch , CURLOPT_USERAGENT , $agent );
+        // оправляем $referer, что пришли с первой страницы сайта
+        curl_setopt ( $ch , CURLOPT_REFERER , $referer );
+        // оправляем на сервер хедер, который мы сами сформировали
+        //curl_setopt ( $ch , CURLOPT_HTTPHEADER , $header );
+        // при получении HTTP заголовка "Location: " будет 
+        // происходить перенаправление
+        curl_setopt ( $ch , CURLOPT_FOLLOWLOCATION , 1 );
+        // запретить проверку сертификата удаленного сервера
+        curl_setopt ( $ch , CURLOPT_SSL_VERIFYPEER, 0 );
+        // не будем проверять существование имени
+       curl_setopt ( $ch , CURLOPT_SSL_VERIFYHOST, 0 );
+    // если есть массив с cookie, то отправим серверу, эти cookie
+        if ( @is_array ($arr_cookie)){
+          while (list($key, $val) = @each ($arr_cookie)){
+            $COKKIES .= trim ($val[0])."=". trim ($val[1])."; ";
+                     }
+              @curl_setopt ( $ch , CURLOPT_COOKIE , $COKKIES." expires=Mon, 14-Apr-08 10:34:13 GMT" );
+                  }
+       // если с сервера пришло cookie, то запишем его в файл $cookie_file
+        //@curl_setopt ( $ch , CURLOPT_COOKIEJAR , $cookie_file );
+        //@curl_setopt ( $ch , CURLOPT_COOKIEFILE , $cookie_file );
+       // если мы послали данные из формы, которая стоит 
+      // на страничке $path, добавляем метод $_POST
+      if (isset($_POST) and $_SERVER["REQUEST_METHOD"]=="POST"){
+              curl_setopt ( $ch , CURLOPT_POST , 1 );
+              curl_setopt ( $ch , CURLOPT_POSTFIELDS , substr ( data_encode ( $_POST ), 0 , - 1 ) );
+               } 
+    // получаем страничку $path с хедером
+   $tmp = curl_exec ( $ch );
+  // закрываем сеанс Curl
+    curl_close ( $ch ); 
+// если Curl ничего не вывел пробуем это сделать output_r();
+// эта функция описана здесь, она использует сокет.
+  if ($tmp==''){ $tmp = output_r ($path); }
+//print $tmp;
+return $tmp;
+}
+  
 ?>
