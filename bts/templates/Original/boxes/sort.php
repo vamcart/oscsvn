f239dc6a9095067fcb42ced565636a5e6405f78c
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2012-05-19 11:26:22 +0000

    Корректировка html-кода

--- bts/templates/Original/boxes/sort.php
+++ bts/templates/Original/boxes/sort.php
@@ -56,7 +56,7 @@ for ($col=0, $n=sizeof($new); $col<$n; $col++) {
 ?>  
 
 <?php
-echo '<a href="href="index.php?'. tep_get_path($current_category_id) .' "target="_self">'.TEXT_ALL.'</a>';
+echo '<a href="index.php?'. tep_get_path($current_category_id) .' "target="_self">'.TEXT_ALL.'</a>';
 ?>
 <br>
 <br>
