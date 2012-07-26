7665a5f540c78d996b7192e970afb9f4769a3d82
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2012-07-26 09:11:38 +0000

    Корректировка sql запроса в фильтрах товара

--- sts/includes/boxes/products_filter.php
+++ sts/includes/boxes/products_filter.php
@@ -23,7 +23,7 @@ if ((isset($_GET['cPath']) && isset($current_category_id)) or FILTERS_MAIN_PAGE 
 
     if ((isset($_GET['cPath']) && isset($current_category_id)) or FILTERS_MAIN_PAGE == 'False') $get_category = "and s2c.categories_id = '" . $current_category_id . "'";
      
-    $specs_query_raw = "select s.specifications_id,
+    $specs_query_raw = "select distinct s.specifications_id,
                                s.products_column_name,
                                s.filter_class,
                                s.filter_show_all,
