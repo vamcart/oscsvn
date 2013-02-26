d795a5c7dcd7b5f3610de41315c75219f2bb8852
oscvam@gmail.com <oscvam@gmail.com@35220d00-f9cb-11de-91aa-85f76f42a988>
2013-02-26 08:00:24 +0000

    Корректировка функций для работы с базой для правильной работы на php 5.5

--- trunk/bts/includes/functions/database.php
+++ trunk/bts/includes/functions/database.php
@@ -14,21 +14,19 @@
     global $$link;
 
     if (USE_PCONNECT == 'true') {
-      $$link = mysql_pconnect($server, $username, $password);
-    } else {
-      $$link = @mysql_connect($server, $username, $password, $new_link);
+      $server = 'p:' . $server;
     }
 
-    if ($$link){
-      @mysql_select_db($database);
-      @mysql_query("SET SQL_MODE= ''");
-      @mysql_query("SET NAMES 'utf8' COLLATE 'utf8_general_ci'");
-    }
+    $$link = mysqli_connect($server, $username, $password, $database);
+
+      @mysqli_query($$link, "SET SQL_MODE= ''");
+      @mysqli_query($$link, "SET NAMES 'utf8' COLLATE 'utf8_general_ci'");
+
 //***VB***
 
 //Start VaM db-error processing
     if (!$$link) {
-      tep_db_error("connect", mysql_errno(), mysql_error());
+      tep_db_error("connect", mysqli_errno($$link), mysqli_error($$link));
     }
 //End VaM db-error processing
     return $$link;
@@ -37,7 +35,7 @@
   function tep_db_close($link = 'db_link') {
     global $$link;
 
-    return mysql_close($$link);
+    return mysqli_close($$link);
   }
 
 //  function tep_db_error($query, $errno, $error) { 
@@ -139,18 +137,18 @@ function zen_trace_back($backtrace=false, $from=0, $to=0, $get_call=true) {
     }
 
      $time_start = explode (' ', microtime());
-     $result = mysql_query($query, $$link);
+     $result = mysqli_query($$link, $query);
      $time_end = explode (' ', microtime());
      $query_time = $time_end[1]+$time_end[0]-$time_start[1]-$time_start[0];
      $query_total_time += $query_time; 
 
     if (defined('STORE_DB_TRANSACTIONS') && (STORE_DB_TRANSACTIONS == 'true')) {
-       $result_error = mysql_error();
+       $result_error = mysqli_error($$link);
        error_log('RESULT ' . $result . ' ' . $result_error . "\n", 3, STORE_PAGE_PARSE_TIME_LOG);
     }
 //Start VaM db-error processing
     if (!$result) {
-      tep_db_error($query, mysql_errno(), mysql_error());
+      tep_db_error($query, mysqli_errno($$link), mysqli_error($$link));
     }
 //End VaM db-error processing
     return $result;
@@ -201,29 +199,29 @@ function zen_trace_back($backtrace=false, $from=0, $to=0, $get_call=true) {
   }
 
   function tep_db_fetch_array($db_query) {
-    return mysql_fetch_array($db_query, MYSQL_ASSOC);
+    return mysqli_fetch_array($db_query, MYSQLI_ASSOC);
   }
 
   function tep_db_num_rows($db_query) {
-    return mysql_num_rows($db_query);
+    return mysqli_num_rows($db_query);
   }
 
   function tep_db_data_seek($db_query, $row_number) {
-    return mysql_data_seek($db_query, $row_number);
+    return mysqli_data_seek($db_query, $row_number);
   }
 
   function tep_db_insert_id($link = 'db_link') { 
     global $$link;
 
-    return mysql_insert_id($$link); 
+    return mysqli_insert_id($$link); 
   }
 
   function tep_db_free_result($db_query) {
-    return mysql_free_result($db_query);
+    return mysqli_free_result($db_query);
   }
 
   function tep_db_fetch_fields($db_query) {
-    return mysql_fetch_field($db_query);
+    return mysqli_fetch_field($db_query);
   }
 
   function tep_db_output($string) {
@@ -231,19 +229,14 @@ function zen_trace_back($backtrace=false, $from=0, $to=0, $get_call=true) {
   }
 
   function tep_db_input($string, $link = 'db_link') {
-  global $$link;
+    global $$link;
 
-  if (function_exists('mysql_real_escape_string')) {
-    return mysql_real_escape_string($string, $$link);
-  } elseif (function_exists('mysql_escape_string')) {
-    return mysql_escape_string($string);
-  }
-  return addslashes($string);
+    return mysqli_real_escape_string($$link, $string);
   }
 
   function tep_db_prepare_input($string) {
     if (is_string($string)) {
-      return trim(tep_sanitize_string(stripslashes($string)));
+      return trim(stripslashes($string));
     } elseif (is_array($string)) {
       reset($string);
       while (list($key, $value) = each($string)) {
@@ -252,6 +245,74 @@ function zen_trace_back($backtrace=false, $from=0, $to=0, $get_call=true) {
       return $string;
     } else {
       return $string;
+    }
+  }
+  
+ if ( !function_exists('mysqli_connect') ) {
+    define('MYSQLI_ASSOC', MYSQL_ASSOC);
+
+    function mysqli_connect($server, $username, $password, $database) {
+      if ( substr($server, 0, 2) == 'p:' ) {
+        $link = mysql_pconnect(substr($server, 2), $username, $password);
+      } else {
+        $link = mysql_connect($server, $username, $password);
+      }
+
+      if ( $link ) {
+        mysql_select_db($database, $link);
+      }
+
+      return $link;
+    }
+
+    function mysqli_close($link) {
+      return mysql_close($link);
+    }
+
+    function mysqli_query($link, $query) {
+      return mysql_query($query, $link);
+    }
+
+    function mysqli_errno($link) {
+      return mysql_errno($link);
+    }
+
+    function mysqli_error($link) {
+      return mysql_error($link);
+    }
+
+    function mysqli_fetch_array($query, $type) {
+      return mysql_fetch_array($query, $type);
+    }
+
+    function mysqli_num_rows($query) {
+      return mysql_num_rows($query);
+    }
+
+    function mysqli_data_seek($query, $offset) {
+      return mysql_data_seek($query, $offsetr);
+    }
+
+    function mysqli_insert_id($link) {
+      return mysql_insert_id($link);
+    }
+
+    function mysqli_free_result($query) {
+      return mysql_free_result($query);
+    }
+
+    function mysqli_fetch_field($query) {
+      return mysql_fetch_field($query);
+    }
+
+    function mysqli_real_escape_string($link, $string) {
+      if ( function_exists('mysql_real_escape_string') ) {
+        return mysql_real_escape_string($string, $link);
+      } elseif ( function_exists('mysql_escape_string') ) {
+        return mysql_escape_string($string);
+      }
+
+      return addslashes($string);
     }
   }
 ?>
